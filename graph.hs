0c423844ddf0a27292f69d42be75df782542182e
Andy Worth <k8m1b@ugrad.cs.ubc.ca>
2021-02-26 14:52:29 -0800

    implemented elemG

--- graph.hs
+++ graph.hs
@@ -3,13 +3,14 @@ module Graph where
 data Graph nodes edges = NewGraph nodes edges
                        deriving Show
 
-insertNode :: (Node name x y) -> (Graph [(Node name x y)] edges) -> (Graph [(Node name x y)] edges)
+insertNode :: Eq name => (Node name x y) -> (Graph [(Node name x y)] edges) -> (Graph [(Node name x y)] edges)
 insertNode node (NewGraph nodes edges)
     | elemG node nodes = NewGraph nodes edges
     | otherwise = NewGraph (node:nodes) edges
 
-elemG :: (Node name x y) -> [(Node name x y)] -> Bool
-elemG (NewNode name x y) (h:t) = False
+elemG :: Eq name => (Node name x y) -> [(Node name x y)] -> Bool
+elemG (NewNode name x y) [] = False
+elemG (NewNode name x y) (h:t) = foldr (\ (NewNode name2 x2 y2) acc -> if (name==name2) then True else acc) False (h:t)
 
 --adds edge containing tuple of node 1 and node 2 as well as the distance between them,
 --only if the edge does not already exist in the graph
