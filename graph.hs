5990a75b1cf920ee687af9236d279e93cb3d96c3
sam.fraser <srfraser@students.cs.ubc.ca>
2021-03-04 17:21:41 -0800

    Final changes and addition of tracking

--- graph.hs
+++ graph.hs
@@ -17,14 +17,17 @@ elemNodes (NewNode name x y) (h:t) = foldr (\ (NewNode name2 x2 y2) acc -> if (n
 
 -- Translates input node information into node objects in the graph
 buildNodes :: [([Char], Double, Double)] -> (Graph [(Node [Char] Double Double)] [((Node [Char] Double Double), (Node [Char] Double Double))]) -> (Graph [(Node [Char] Double Double)] [((Node [Char] Double Double), (Node [Char] Double Double))])
+buildNodes [] (NewGraph nodes edges) = (NewGraph nodes edges)
 buildNodes ((c, d1, d2):t) (NewGraph nodes edges) = foldr (\ n acc -> (insertNode n acc)) (NewGraph nodes edges) [(NewNode c d1 d2) | (c, d1, d2) <- ((c, d1, d2):t)]
 
 -- Adds edges between all the nodes in a graph
 buildEdges :: (Eq c) => (Graph [(Node [c] Double Double)] [((Node [c] Double Double), (Node [c] Double Double))]) -> (Graph [(Node [c] Double Double)] [((Node [c] Double Double), (Node [c] Double Double))])
+buildEdges (NewGraph [] edges) = (NewGraph [] edges)
 buildEdges (NewGraph nodes edges) = foldr (\ n acc -> (buildEdgesForNode n acc)) (NewGraph nodes edges) nodes
 
 -- Adds edges between a node and all nodes but itself
 buildEdgesForNode :: (Eq c) => (Node [c] Double Double) -> (Graph [(Node [c] Double Double)] [((Node [c] Double Double), (Node [c] Double Double))]) -> (Graph [(Node [c] Double Double)] [((Node [c] Double Double), (Node [c] Double Double))])
+buildEdgesForNode (NewNode name x y) (NewGraph [] edges) = (NewGraph [] edges)
 buildEdgesForNode (NewNode name x y) (NewGraph nodes edges) = foldr (\ n acc -> (insertEdge (NewNode name x y) n acc)) (NewGraph nodes edges) nodes
 
 -- Adds an edge between the two input nodes to the input graph, it it wasn't already added
@@ -48,6 +51,7 @@ buildWeightedGraph ((c, d1, d2):t) = buildWeights (buildGraph ((c, d1, d2):t))
 
 -- Iterates through the edges and calls insertWeight
 buildWeights :: (Graph [(Node [Char] Double Double)] [((Node [Char] Double Double), (Node [Char] Double Double))]) -> (Graph [(Node [Char] Double Double)] [((Node [Char] Double Double), (Node [Char] Double Double), Double)])
+buildWeights (NewGraph nodes []) = (NewGraph nodes [])
 buildWeights (NewGraph nodes edges) = foldr (\ e (NewGraph n wedges) -> (NewGraph n ((insertWeight e):wedges))) (NewGraph nodes []) edges
 
 -- Inserts a weight for a given edge
@@ -64,6 +68,7 @@ getWeight (NewNode name1 x1 y1) (NewNode name2 x2 y2) = dist x1 y1 x2 y2
 
 -- Finds the closest node to the input node, that isn't itself
 closest :: (Node [Char] Double Double) -> (Graph [(Node [Char] Double Double)] [((Node [Char] Double Double), (Node [Char] Double Double), Double)]) -> (Node [Char] Double Double)
+closest (NewNode name1 x1 y1) (NewGraph [] edges) = (NewNode "error" 0 0)
 closest (NewNode name1 x1 y1) (NewGraph nodes edges) = foldr (\ (NewNode name2 x2 y2) acc -> if ((name1/=name2) && ((getWeight (NewNode name1 x1 y1) (NewNode name2 x2 y2))<(getWeight (NewNode name1 x1 y1) acc))) then (NewNode name2 x2 y2) else acc) (NewNode "error" 2147483647 2147483647) nodes
 
 -- Given an address, constructs a Node including that address and its lat/lon coordinates
--- main.hs
+++ main.hs
@@ -17,11 +17,19 @@ import Graph
 
 -- Gets the name of all the given nodes
 getNameofNodes :: [(Node [Char] Double Double)] -> [Char]
-getNameofNodes ((NewNode name1 x1 y1):t) = foldr (\ (NewNode name2 x2 y2) acc -> name2 ++ "|" ++ acc) [] ((NewNode name1 x1 y1):t)
+getNameofNodes [] = []
+getNameofNodes ((NewNode name1 x1 y1):t) = foldr (\ (NewNode name2 x2 y2) acc -> name2 ++ "\n" ++ acc) [] ((NewNode name1 x1 y1):t)
 
 -- Gets a node given the name
 getNodeofNames :: [Char] -> [(Node [Char] Double Double)] -> (Node [Char] Double Double)
+getNodeofNames str [] = (NewNode "error" 0 0)
 getNodeofNames str ((NewNode name1 x1 y1):t) = foldr (\ (NewNode name2 x2 y2) acc -> if (str==name2) then (NewNode name2 x2 y2) else acc) (NewNode "error" 0 0) ((NewNode name1 x1 y1):t)
+
+-- Gets the total distance of an ordered list of nodes
+totalDistance :: [(Node [Char] Double Double)] -> Double
+totalDistance [] = 0
+totalDistance [a] = 0
+totalDistance (node1:node2:t) = (getWeight node1 node2) + totalDistance (node2:t)
 
 ask q =
     do
@@ -29,25 +37,34 @@ ask q =
         line <- getLine
         return line
 
--- Introduction to IO, builds graph, welcomes user, and fetches possible destinations. User then selects their first destination, and they enter the recursive navigation loop.
-waypoint =
-   do
-       let (NewGraph nodes edges) = buildWeightedGraph [("Musqueam Welcome Post", 49.265646111631696, -123.25041373987455), ("Main Mall", 49.26482348106154, -123.25277587971838), ("Brock Hall", 49.268883898466314, -123.25201564267893), ("UBC Aquatic Centre", 49.26782641555527, -123.24874922684376), ("The Nest", 49.26640483638247, -123.24926326761386), ("Orchard Commons Residence", 49.260174961420255, -123.25094001971785), ("Nitobe Memorial Garden", 49.2665725529049, -123.25959303959296), ("UBC Rose Garden", 49.26955940896691, -123.25648431676166)]
-       let nodeNames = getNameofNodes nodes
-       line <- ask ("Select the next Waypoint on your journey:\n\n" ++ nodeNames ++ "\nExample Input: \"Main Mall\"\n[Type stop at anytime to quit]\n> ")
-       if (line=="stop")
-       then
-         return "You have decided to stop. Thank you for traveling with Waypoint!"
-       else do
-          let currNode = getNodeofNames line nodes
-          putStr ("\nYou have arrived at " ++ line ++ "!")
-          let (NewNode name x1 x2) = closest currNode (NewGraph nodes edges)
-          putStr ("\nYour next closest Waypoint is: " ++ name ++ "!\n\n")
-          waypoint
+{-- Introduction via IO, builds graph, welcomes user, and fetches possible destinations. 
+ User then selects their first destination, and they enter the recursive navigation loop.
+ Tracks user pathing and returns path with total distance upon exit. --}
+waypoint :: [(Node [Char] Double Double)] -> IO [Char]
+waypoint path =
+    do
+        let (NewGraph nodes edges) = buildWeightedGraph [("Musqueam Welcome Post", 49.265646111631696, -123.25041373987455), ("Main Mall", 49.26482348106154, -123.25277587971838), ("Brock Hall", 49.268883898466314, -123.25201564267893), ("UBC Aquatic Centre", 49.26782641555527, -123.24874922684376), ("The Nest", 49.26640483638247, -123.24926326761386), ("Orchard Commons Residence", 49.260174961420255, -123.25094001971785), ("Nitobe Memorial Garden", 49.2665725529049, -123.25959303959296), ("UBC Rose Garden", 49.26955940896691, -123.25648431676166)]
+        let nodeNames = getNameofNodes nodes
+        line <- ask ("Select the next Waypoint on your journey:\n\n" ++ nodeNames ++ "\nExample Input: \"Main Mall\"\n[Type stop at anytime to quit]\n> ")
+        if (line=="stop")
+        then do
+            putStr ("\nTour Path:\n" ++ (getNameofNodes path) ++ "\nTotal Coordinate Distance: " ++ (show (totalDistance path)) ++ "\n\n")
+            return "You have decided to stop. Thank you for traveling with Waypoint!"
+        else do
+            let (NewNode currName currX currY) = getNodeofNames line nodes
+            if (currName == "error")
+                then do
+                    putStr ("\nYou have entered an invalid Waypoint, please try again.\n")
+                    waypoint path
+                else do
+                    putStr ("\nYou have arrived at " ++ line ++ "!")
+                    let (NewNode name x1 x2) = closest (NewNode currName currX currY) (NewGraph nodes edges)
+                    putStr ("\nYour next closest Waypoint is: " ++ name ++ "!\n\n")
+                    waypoint (path ++ [(NewNode currName currX currY)])
 
 -- Main entry point to program
 go :: IO [Char]
 go =
   do
     putStr ("\n | Welcome to the Waypoint UBC Map! |\n")
-    waypoint
+    waypoint []
